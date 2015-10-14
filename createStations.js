e04830fcb8fe1249c01f72a96335643a72a433d6
Peter Larsen <petertretorn@gmail.com>
2015-10-14 19:01:06 +0200

    mongo script changed

--- createStations.js
+++ createStations.js
@@ -4,6 +4,22 @@ var mongoose = require('mongoose');
 var Station = mongoose.model('Station');
 var PriceData = mongoose.model('PriceData');
 
+function purgeData() {
+	mongoose.connect('mongodb://localhost/travelapp');
+
+	console.log('inside purgeData');
+
+	Station.remove({}, function(err) {
+		if (err) console.log('failed to remove stations!');
+	});
+
+
+	PriceData.remove({}, function(err) {
+		if (err) console.log('failed to remove pricedata');
+	});
+}
+
+
 function createStations() {
 	var stations = ['Aalborg', 'Odense', 'Århus', 'Copenhagen', 'Svendborg', 'Randers', 'Thisted', 'Esbjerg'];
 
@@ -17,16 +33,57 @@ function createStations() {
 	})
 }
 
-//createStations();
-
-var pd = new PriceData({ from: 'Aalborg', to: 'Copenhagen', price: 542 });
-pd.save(function(err) {
-	if (err) console.log('failed');
-	else console.log('success');
-})
-
 function createPriceData() {
+	Station.find({}, function(err, stations) {
+		if (err) {
+			console.log('failed to fetch stations');
+			process.exit(0);
+		}
+		
+		var stationNames = stations.map(function(station) {
+			return station.name;
+		});
 
+		for (var i = 0; i < stationNames.length; i++) {
+			var from = stationNames[i];
+
+			for (var j = i; j < stationNames.length; j++) {
+				if (i == j) continue;
+
+				var to = stationNames[j];
+
+				var price = getRandomInt(100, 600);
+
+				var priceData = new PriceData({ from: from, to: to, price: price});
+
+				priceData.save(function(err) {
+					if (err) console.log('failed');
+				})
+			}			
+		}
+	})
 }
 
-createPriceData();
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function test() {
+	console.log('just testing...');
+}
+
+mongoose.connection.close();
+
+ //purgeData()
+
+ //createStations()
+
+ //createPriceData()
+
+ module.exports = {
+ 	purgeData: purgeData,
+ 	createStations: createStations,
+ 	createPriceData: createPriceData,
+ 	test: test
+ }
+ 
--- routes/priceRoute.js
+++ routes/priceRoute.js
@@ -8,33 +8,12 @@ router.get('/', function(req, res, next) {
 	var from = req.query.from,
 		to = req.query.to;
 
-		console.log('from: %s, to: %s', from, to);
+	console.log('from: %s, to: %s', from, to);
 
-		//price = getPrice(from, to);
-
-		PriceData.find({ from: from, to: to}, function(err, price) {
-			if (err) return next(err);
-			console.log('inside cb');
-			res.json(price);
-		});
-
-		//console.log('price is: %d', price);
-
-		//res.json({ message: 'no match!'});
+	PriceData.find({ $or: [ { from: from, to: to }, { from: to, to: from }] }, function(err, price) {
+		if (err) return next(err);
+		res.json(price);
+	});
 })
-
-function getPrice(from, to) {
-      
-	  var priceMatrix = {
-	    Aalborg: { Aarhus: 175, Odense: 225, Copenhagen: 340, Randers: 150, Roskilde : 310 },
-	    Aarhus: { Aalborg: 175, Odense: 225, Copenhagen: 340, Randers: 150, Roskilde : 310 },
-	    Odense: { Aarhus: 175, Aalborg: 225, Copenhagen: 340, Randers: 150, Roskilde : 310 },
-	    Copenhagen: { Aarhus: 175, Odense: 225, Aalborg: 340, Randers: 150, Roskilde : 55 },
-	    Randers: { Aarhus: 175, Odense: 225, Aalborg: 340, Copenhagen: 150, Roskilde : 310 },
-	    Roskilde: { Aarhus: 175, Odense: 225, Aalborg: 340, Randers: 150, Copenhagen : 55 }
-	  }
-
-	  return priceMatrix[from][to];
-  }
 
 module.exports = router;
